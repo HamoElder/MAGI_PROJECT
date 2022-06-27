0727f9ff86b0428c52b898049d05b42f47380ee0
HamoElder <missdown123@gmail.com>
2022-06-27 21:02:16 +0800

    ScEq Completed

--- src/main/scala/magiRF/packages/Equalizer/SCEqualizer.scala
+++ src/main/scala/magiRF/packages/Equalizer/SCEqualizer.scala
@@ -1,6 +1,7 @@
 package magiRF.packages.Equalizer
 
 import Misc.math.Complex
+import magiRF.top.IEEE802_11.IEEE802_11
 import spinal.core._
 import spinal.lib._
 import utils.bus.IQBundle.IQBundle
@@ -9,18 +10,19 @@ case class SCEqualizer(
                           dataTypePeak          : ExpNumber = 8 exp,
                           dataTypeResolution    : ExpNumber = -2 exp,
                           refSeqArray           : Array[Complex],
-                          iterations: Int
+                          divisionFactor        : Int = -1,
+                          iterations            : Int
                       ) extends Component{
     def dataWidth: Int = dataTypePeak.value - dataTypeResolution.value + 1
     def seqSize: Int = refSeqArray.length
     def dataType: SInt = SInt(dataWidth bits)
     def cntDataWidth: Int = log2Up(seqSize)
     def cntDataType: UInt = UInt(cntDataWidth bits)
+    def divFactor: Int = if(divisionFactor < 0) log2Up(seqSize) else divisionFactor
+    println(divFactor)
     val io = new Bundle{
-        val raw_data = slave(Flow(IQBundle(dataType)))
-        val equalized_data = master(Flow(IQBundle(dataType)))
-        val train_en = in(Bool())
-        val train_finish = out(Bool())
+        val raw_data = slave(Stream(Fragment(IQBundle(dataType))))
+        val equalized_data = master(Flow(Fragment(IQBundle(dataType))))
     }
     noIoPrefix()
 
@@ -35,14 +37,113 @@ case class SCEqualizer(
         cha_q_ref(idx) := refSeqArray(idx).im.toInt
     }
 
+
     val cnt = Reg(cntDataType) init(0)
-    when(io.train_en){
+    val train_en = Reg(Bool()) init(True)
 
+    when(io.raw_data.last){
+        cnt := 0
     }.elsewhen(io.raw_data.fire){
-        cnt := cnt + 1
-    }.otherwise{
+        cnt := (cnt === (seqSize - 1)) ? U(seqSize - 1) | (cnt + 1)
+    }
 
+    when(io.equalized_data.last){
+        train_en := True
+    }.elsewhen(cnt === (seqSize - 1) && io.raw_data.fire){
+        train_en := False
     }
 
 
+    val i_zf_eq = ZeroForcing(dataTypePeak, dataTypeResolution, iterations)
+    i_zf_eq.io.raw_data.valid := io.raw_data.valid
+    i_zf_eq.io.raw_data.payload := io.raw_data.cha_i
+    i_zf_eq.io.scale := cha_i_scale
+    i_zf_eq.io.train_en := train_en
+    i_zf_eq.io.ref_data := cha_i_ref(cnt)
+    val q_zf_eq = ZeroForcing(dataTypePeak, dataTypeResolution, iterations)
+    q_zf_eq.io.raw_data.valid := io.raw_data.valid
+    q_zf_eq.io.raw_data.payload := io.raw_data.cha_q
+    q_zf_eq.io.scale := cha_q_scale
+    q_zf_eq.io.train_en := train_en
+    q_zf_eq.io.ref_data := cha_q_ref(cnt)
+
+    val train_cnt = Reg(cntDataType) init(0)
+    val train_finished = Reg(Bool()) init(False)
+    when(io.equalized_data.last){
+        train_cnt := 0
+        train_finished := False
+        cha_i_scale := 0
+    }.elsewhen((i_zf_eq.io.result_data.valid && q_zf_eq.io.result_data.valid) && !(train_finished)){
+        train_cnt := train_cnt + 1
+        when(train_cnt === (seqSize - 1)){
+            train_finished := True
+        }
+        cha_i_scale := cha_i_scale + (i_zf_eq.io.result_data.payload |>> divFactor)
+        cha_q_scale := cha_q_scale +  (q_zf_eq.io.result_data.payload |>> divFactor)
+    }
+    val raw_data_ready = Reg(Bool()) init(True)
+    when(io.raw_data.last && io.raw_data.fire){
+        raw_data_ready := False
+    }.elsewhen(io.equalized_data.last && io.equalized_data.fire){
+        raw_data_ready := True
+    }
+    io.raw_data.ready := raw_data_ready
+
+    io.equalized_data.valid := train_finished ? (i_zf_eq.io.result_data.valid && q_zf_eq.io.result_data.valid) | False
+    io.equalized_data.cha_i := train_finished ? i_zf_eq.io.result_data.payload | S(0)
+    io.equalized_data.cha_q := train_finished ? q_zf_eq.io.result_data.payload | S(0)
+    io.equalized_data.last := Delay(io.raw_data.last, iterations)
+}
+
+
+
+object SCEqualizerSimApp extends App {
+    import spinal.core.sim._
+    val ref_data = IEEE802_11.ltfFreq.map(i=>{i*(1 << 7)})
+    println(ref_data.mkString("Array(", ", ", ")"))
+    SimConfig.withWave.allOptimisation
+        .doSim(new OFDMEqualizer(3 exp, -12 exp, ref_data,
+            16)) { dut =>
+            dut.clockDomain.forkStimulus(5)
+            dut.io.raw_data.valid #= false
+            dut.io.raw_data.last #= false
+            dut.clockDomain.waitSampling(10)
+
+            /**
+             * Train
+             */
+            for(data <- ref_data){
+                dut.io.raw_data.cha_i #= (data.re*0.707).toInt
+                dut.io.raw_data.cha_q #= (data.im*0.618).toInt
+                dut.io.raw_data.valid #= true
+                // dut.io.raw_data.valid.randomize()
+                dut.clockDomain.waitSampling(1)
+            }
+
+            /**
+             * Eq
+             */
+            for(data <- ref_data){
+                dut.io.raw_data.cha_i #= (data.re*0.707).toInt
+                dut.io.raw_data.cha_q #= (data.im*0.618).toInt
+                dut.io.raw_data.valid #= true
+                // dut.io.raw_data.valid.randomize()
+                dut.clockDomain.waitSampling(1)
+            }
+            //            for(idx <- 1 until 128){
+            //                dut.io.raw_data.cha_i #= (idx << 7)
+            //                dut.io.raw_data.cha_q #= (idx << 5)
+            //                dut.io.raw_data.valid #= true
+            //                // dut.io.raw_data.valid.randomize()
+            //                dut.clockDomain.waitSampling(1)
+            //            }
+            dut.io.raw_data.cha_i #= 256
+            dut.io.raw_data.cha_q #= 256
+            dut.io.raw_data.valid #= true
+            dut.io.raw_data.last #= true
+            dut.clockDomain.waitSampling(1)
+            dut.io.raw_data.valid #= false
+            dut.io.raw_data.last #= false
+            dut.clockDomain.waitSampling(50)
+        }
 }
--- src/main/scala/magiSOC/perips/DMA/SGDMA/SGDMACore.scala
+++ src/main/scala/magiSOC/perips/DMA/SGDMA/SGDMACore.scala
@@ -1,7 +1,11 @@
 package magiSOC.perips.DMA.SGDMA
 
+import magiSOC.perips.DMA.BlockDMA.{BDMAConfig, BDMACore}
 import spinal.core._
 import spinal.lib._
+import spinal.lib.bus.amba4.axi.{Axi4, Axi4SpecRenamer}
+import utils.bus.AxiStream4.{AxiStream4, AxiStream4SpecRenamer}
+
 
 case class SGDMACoreConfig(
                               sgAddrWidth    : Int = 32,
@@ -22,14 +26,76 @@ case class SGDMACoreConfig(
                               outStandingLen : Int     = 2,
                               endianness     : Endianness = LITTLE
                           ){
-
+    require(sgDataWidth == 32 || sgDataWidth == 64, "Data width must be 32 or 64")
+    def sgAddrDataType: UInt = UInt(sgAddrWidth bits)
+    def sgDataType: Bits = Bits(sgDataWidth bits)
+    def sgLinkedListDepth: Int = 32
+    val bdmaConfig: BDMAConfig = BDMAConfig(axi4AddrWidth, axi4DataWidth, axi4MaxBurstLen, axi4IDWidth, axis4StrbEn, axis4KeepEn,
+        axis4IDEn, axis4LastEn, bytesLimit, outStandingLen, endianness)
 }
 
+object SGDMACCHStates extends SpinalEnum{
+    val IDLE, CHECK_PREAMBLE, GET_MSG, SET_NEXT_PTR = newElement()
+}
+
+case class SGDMAControlChannel(config:SGDMACoreConfig) extends Bundle with IMasterSlave{
+    val sg_startup_addr = UInt(config.sgAddrWidth bits)
+    val sg_reset = Bool()
+    val sg_error = Bool()
+    /**
+     * Error :Preamble Not Match
+     */
+    override def asMaster(): Unit = {
+        out(sg_startup_addr, sg_reset)
+        in(sg_error)
+    }
+
+    override type RefOwnerType = this.type
+}
 
 case class SGDMACore(config: SGDMACoreConfig) extends Component {
     val io = new Bundle{
+        val axi4S2M = master(Axi4(config.bdmaConfig.axi4Config))
+        val axi4M2S = master(Axi4(config.bdmaConfig.axi4Config))
+
+        val dataS2M = slave(AxiStream4(config.bdmaConfig.axisConfig))
+        val dataM2S = master(AxiStream4(config.bdmaConfig.axisConfig))
+
+        val intr = out(Bool())
+
+        val sgCtrl = slave(Stream(SGDMAControlChannel(config)))
 
     }
     noIoPrefix()
+    Axi4SpecRenamer(io.axi4M2S)
+    Axi4SpecRenamer(io.axi4S2M)
+    AxiStream4SpecRenamer(io.dataS2M)
+    AxiStream4SpecRenamer(io.dataM2S)
+
+    val bdma_code = BDMACore(config.bdmaConfig)
+    val linked_list_cursor = Reg(UInt(config.sgAddrWidth bits)) init(0)
+
+
+    val sg_cch_state = Reg(SGDMACCHStates()) init(SGDMACCHStates.IDLE)
+    switch(sg_cch_state){
+        is(SGDMACCHStates.IDLE){
+            when(io.sgCtrl.fire){
+                sg_cch_state := SGDMACCHStates.CHECK_PREAMBLE
+            }
+        }
+        is(SGDMACCHStates.CHECK_PREAMBLE){
+
+        }
+        is(SGDMACCHStates.GET_MSG){
+
+        }
+    }
+    /** 32 bits
+     * Symbol(16 bits) | desc_burst(2 bits)|desc_id(8 bits)|type(1 bits) | finish(1 bits)
+     * start_addr(axi4AddrWidth bits)
+     * desc_total_bytes(axi4DataWidth bits)
+     * nextPtrAddr(sgAddrWidth bits)
+     */
+
 
 }
